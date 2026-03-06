4426ffa8acad8fbb965dc4244a6e3fe1372181d3
Eli Hini <8249954+EliHini@users.noreply.github.com>
2026-03-06 14:21:32 -0800

    Password Detail View finalized
    No edits
    No delete
    
    Signed-off-by: Eli Hini <8249954+EliHini@users.noreply.github.com>

--- ios/brave-ios/Sources/BraveStrings/BraveStrings.swift
+++ ios/brave-ios/Sources/BraveStrings/BraveStrings.swift
@@ -9468,5 +9468,13 @@ extension Strings {
         value: "Enter Password",
         comment: "Placeholder for the password field when adding or editing a login"
       )
+    public static let managePasswordDetailsRevealPassword =
+      NSLocalizedString(
+        "autofill.managePasswords.passwordDetail.revealPassword",
+        tableName: "BraveShared",
+        bundle: .module,
+        value: "Reveal",
+        comment: "Reveal password label"
+      )
   }
 }
