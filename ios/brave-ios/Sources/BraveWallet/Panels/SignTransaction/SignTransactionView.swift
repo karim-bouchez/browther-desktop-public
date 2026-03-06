3ba9144197cb12092e407d2fc3ee309cc1cb6913
Nuo Xu <nuoxu.nx@gmail.com>
2026-03-06 12:02:05 -0500

    Add a warning before user sign message for certain requests.

--- ios/brave-ios/Sources/BraveWallet/Panels/SignTransaction/SignTransactionView.swift
+++ ios/brave-ios/Sources/BraveWallet/Panels/SignTransaction/SignTransactionView.swift
@@ -33,7 +33,6 @@ struct SignTransactionView: View {
   @State private var showWarning: Bool = true
   @Environment(\.sizeCategory) private var sizeCategory
   @Environment(\.colorScheme) private var colorScheme
-  @Environment(\.openURL) private var openWalletURL
   @ScaledMetric private var blockieSize = 54
   private let maxBlockieSize: CGFloat = 108
 
@@ -245,7 +244,7 @@ struct SignTransactionView: View {
         }
         .padding(.horizontal, 8)
         if showWarning {
-          warningView
+          SignMessageWarningView()
             .padding(.vertical, 12)
             .padding(.horizontal, 20)
         } else {
@@ -382,7 +381,19 @@ struct SignTransactionView: View {
     }
   }
 
-  @ViewBuilder private var warningView: some View {
+  private func next() {
+    if txIndex + 1 < requests.count {
+      txIndex += 1
+    } else {
+      txIndex = 0
+    }
+  }
+}
+
+struct SignMessageWarningView: View {
+  @Environment(\.openURL) private var openWalletURL
+
+  var body: some View {
     VStack(alignment: .leading, spacing: 8) {
       Group {
         Label(Strings.Wallet.signTransactionSignRisk, systemImage: "exclamationmark.triangle")
@@ -407,14 +418,6 @@ struct SignTransactionView: View {
       Color(.braveErrorBackground)
         .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
     )
-  }
-
-  private func next() {
-    if txIndex + 1 < requests.count {
-      txIndex += 1
-    } else {
-      txIndex = 0
-    }
   }
 }
 
