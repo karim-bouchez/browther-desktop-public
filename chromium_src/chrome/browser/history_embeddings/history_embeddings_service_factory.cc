brave/brave-core

Replace history_embeddings patch with template override
Replace the trivial patch that commented out scored_url_rows.clear()
with a BraveHistoryEmbeddingsService template subclass (per
docs/gni_sources.md) that overrides OnPassageVisibilityCalculated to
always synthesize passing visibility scores (1.0), bypassing upstream's
PageContentAnnotationsService-based filtering which Brave doesn't use.

The template pattern avoids circular deps between
//brave/browser/history_embeddings and //chrome/browser:browser —
the template is instantiated at the factory site which already has
the required dep.

chromium_src/chrome/browser/history_embeddings/history_embeddings_service_factory.cc
@@ -5,13 +5,22 @@
 
 #include "chrome/browser/history_embeddings/history_embeddings_service_factory.h"
 
+#include "brave/browser/history_embeddings/brave_history_embeddings_service.h"
 #include "brave/browser/history_embeddings/brave_passage_embeddings_service_controller.h"
+#include "chrome/browser/history_embeddings/chrome_history_embeddings_service.h"
 #include "chrome/browser/passage_embeddings/chrome_passage_embeddings_service_controller.h"
 
 // Replace Chrome's PassageEmbeddingsServiceController with Brave's
 // Both are in the passage_embeddings namespace, so direct replacement works
 #define ChromePassageEmbeddingsServiceController \
   BravePassageEmbeddingsServiceController
+
+// Include the chrome header above so the class is declared before renaming.
+// The #define only affects make_unique calls in the .cc body.
+// See docs/gni_sources.md for the template pattern.
+#define ChromeHistoryEmbeddingsService \
+  BraveHistoryEmbeddingsService<       \
+      history_embeddings::ChromeHistoryEmbeddingsService>
 
 // Override GetEmbedder() call to return our BraveEmbedder for the given profile
 // instead of base class's embedder_. This passes the profile to create
@@ -21,4 +30,5 @@
 #include <chrome/browser/history_embeddings/history_embeddings_service_factory.cc>
 
 #undef GetEmbedder
+#undef ChromeHistoryEmbeddingsService
 #undef ChromePassageEmbeddingsServiceController

karim-bouchez/browther-desktop-public

chromium_src/chrome/browser/history_embeddings/history_embeddings_service_factory.cc
@@ -6,12 +6,21 @@
 #include "chrome/browser/history_embeddings/history_embeddings_service_factory.h"
 
 #include "brave/browser/history_embeddings/brave_passage_embeddings_service_controller.h"
+#include "brave/components/history_embeddings/brave_history_embeddings_service.h"
+#include "chrome/browser/history_embeddings/chrome_history_embeddings_service.h"
 #include "chrome/browser/passage_embeddings/chrome_passage_embeddings_service_controller.h"
 
 // Replace Chrome's PassageEmbeddingsServiceController with Brave's
 // Both are in the passage_embeddings namespace, so direct replacement works
 #define ChromePassageEmbeddingsServiceController \
   BravePassageEmbeddingsServiceController
+
+// Include the chrome header above so the class is declared before renaming.
+// The #define only affects make_unique calls in the .cc body.
+// See docs/gni_sources.md for the template pattern.
+#define ChromeHistoryEmbeddingsService \
+  BraveHistoryEmbeddingsService<       \
+      history_embeddings::ChromeHistoryEmbeddingsService>
 
 // Override GetEmbedder() call to return our BraveEmbedder for the given profile
 // instead of base class's embedder_. This passes the profile to create
@@ -21,4 +30,5 @@
 #include <chrome/browser/history_embeddings/history_embeddings_service_factory.cc>
 
 #undef GetEmbedder
+#undef ChromeHistoryEmbeddingsService
 #undef ChromePassageEmbeddingsServiceController